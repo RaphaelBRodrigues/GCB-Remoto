4c791d74b957d0e8859899822d11b9524df375d5
raphaelbrodrigues <raphaelbarbosa.rodrigues@gmail.com>
2020-09-19 13:11:12 -0300

    Configurando a aplicação para a hospedagem

--- node/database/connection.js
+++ node/database/connection.js
@@ -1,14 +1,25 @@
 const Knex = require("knex");
 
 
+// const connection = new Knex({
+//     client:"mysql2",
+//     connection:{
+//         host : '127.0.0.1',
+//         user : 'root',
+//         password : 'root',
+//         database : 'GCB'
+//     }
+// });
+
 const connection = new Knex({
     client:"mysql2",
     connection:{
-        host : '127.0.0.1',
+        host : 'mysql669.umbler.com',
         user : 'root',
-        password : 'root',
-        database : 'GCB'
+        password : 'raphaelgcb',
+        database : 'umblerroota'
     }
 });
+
 
 module.exports = connection;
